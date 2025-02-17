eeabf713f7dbd2452cea12bd7ea68b27e6f5b8e0
LalithaDevadass <lalitha_ramesh@hotmail.com>
2025-02-17 15:56:09 -0600

    Update streamlit_app.py
    added JSON Data Inside the Response Object

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 
 
@@ -56,4 +53,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
     
-
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json ())
